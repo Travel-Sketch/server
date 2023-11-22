dd508ff69d6f57b0a73cf07c0932ecce2d462bca
Chaos0103 <lyt1228@naver.com>
2023-11-22 22:59:26 +0900

    feat: 닉네임 변경 API 스켈레톤 코드 생성

--- src/main/java/com/travelsketch/travel/api/controller/member/MemberController.java
+++ src/main/java/com/travelsketch/travel/api/controller/member/MemberController.java
@@ -1,13 +1,19 @@
 package com.travelsketch.travel.api.controller.member;
 
 import com.travelsketch.travel.api.ApiResponse;
+import com.travelsketch.travel.api.controller.member.request.ModifyNicknameRequest;
 import com.travelsketch.travel.api.controller.member.request.ModifyPwdRequest;
+import com.travelsketch.travel.api.controller.member.response.ModifyNicknameResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+import static com.travelsketch.travel.api.ApiResponse.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,6 +23,15 @@ public class MemberController {
 
     @PatchMapping("/pwd")
     public ApiResponse<String> modifyPwd(@RequestBody ModifyPwdRequest request) {
-        return ApiResponse.ok(null);
+        return ok(null);
+    }
+
+    @PatchMapping("/nickname")
+    public ApiResponse<ModifyNicknameResponse> modifyNickname(@RequestBody ModifyNicknameRequest request) {
+        ModifyNicknameResponse response = ModifyNicknameResponse.builder()
+            .modifiedNickname("에스파 카리나")
+            .modifiedDate(LocalDateTime.of(2023, 11, 11, 10, 0))
+            .build();
+        return ok(response);
     }
 }
