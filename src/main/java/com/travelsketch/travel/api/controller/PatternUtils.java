cdc87046dde7cca5ee959923e71767a00434e1f5
Chaos0103 <lyt1228@naver.com>
2023-11-24 14:32:17 +0900

    feat: 회원 가입 데이터 검증 정규식 구현

---
+++ src/main/java/com/travelsketch/travel/api/controller/PatternUtils.java
@@ -0,0 +1,27 @@
+package com.travelsketch.travel.api.controller;
+
+import static java.util.regex.Pattern.*;
+
+public abstract class PatternUtils {
+
+    private static final String EMAIL = "\\w+@\\w+\\.\\w+(\\.\\w+)?";
+    private static final String PWD = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\\W]).+$";
+    private static final String KOREAN = "^[가-힣]*$";
+    private static final String NICKNAME = "^[a-zA-Z가-힣0-9]*$";
+
+    public static boolean isEmail(String text) {
+        return matches(EMAIL, text);
+    }
+
+    public static boolean isPwdPattern(String text) {
+        return matches(PWD, text);
+    }
+
+    public static boolean isNicknamePattern(String nickname) {
+        return matches(NICKNAME, nickname);
+    }
+
+    public static boolean isKorean(String text) {
+        return matches(KOREAN, text);
+    }
+}
