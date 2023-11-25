cb3abae5111ff11f161e1f0402843aa37d49f857
Chaos0103 <lyt1228@naver.com>
2023-11-25 17:51:29 +0900

    feat: 스프링 시큐리티 설정

--- src/main/java/com/travelsketch/travel/config/SecurityConfig.java
+++ src/main/java/com/travelsketch/travel/config/SecurityConfig.java
@@ -1,16 +1,51 @@
 package com.travelsketch.travel.config;
 
+import com.travelsketch.travel.security.JwtAuthenticationFilter;
+import com.travelsketch.travel.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable));
+
+        http.authorizeHttpRequests(
+            authorize -> authorize
+                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                .requestMatchers(new MvcRequestMatcher(introspector, "/api/v1/join")).permitAll()
+                .requestMatchers(new MvcRequestMatcher(introspector, "/api/v1/login")).permitAll()
+                .requestMatchers(new MvcRequestMatcher(introspector, "/**")).hasRole("USER")
+                .anyRequest().authenticated()
+        );
+
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
