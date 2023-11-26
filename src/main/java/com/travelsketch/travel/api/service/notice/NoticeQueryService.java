88bdf57bdcc262f86e1e98a4de3b4162489b7701
Chaos0103 <lyt1228@naver.com>
2023-11-27 01:48:49 +0900

    feat: 공지사항 상세 조회 서비스 구현

--- src/main/java/com/travelsketch/travel/api/service/notice/NoticeQueryService.java
+++ src/main/java/com/travelsketch/travel/api/service/notice/NoticeQueryService.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -30,6 +32,16 @@ public class NoticeQueryService {
     }
 
     public NoticeDetailResponse searchById(Long noticeId) {
-        return null;
+        Optional<NoticeDetailResponse> findResponse = noticeQueryRepository.findById(noticeId);
+        if (findResponse.isEmpty()) {
+            throw new NoSuchElementException("등록되지 않은 공지사항입니다.");
+        }
+        NoticeDetailResponse response = findResponse.get();
+
+        if (response.getIsDeleted()) {
+            throw new IllegalArgumentException("삭제된 공지사항입니다.");
+        }
+
+        return response;
     }
 }
--- src/main/java/com/travelsketch/travel/domain/notice/repository/NoticeQueryRepository.java
+++ src/main/java/com/travelsketch/travel/domain/notice/repository/NoticeQueryRepository.java
@@ -79,6 +79,7 @@ public class NoticeQueryRepository {
                     Expressions.asNumber(noticeId),
                     notice.title,
                     notice.content,
+                    notice.isDeleted,
                     notice.createdDate
                 )
             )
