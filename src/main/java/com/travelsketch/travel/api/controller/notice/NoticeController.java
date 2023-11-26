c40da85b96e17ca8a65a154880049b06133bcd7d
Chaos0103 <lyt1228@naver.com>
2023-11-26 17:01:03 +0900

    test: 공지사항 수정 API docs 테스트 코드 구현

--- src/main/java/com/travelsketch/travel/api/controller/notice/NoticeController.java
+++ src/main/java/com/travelsketch/travel/api/controller/notice/NoticeController.java
@@ -2,13 +2,17 @@ package com.travelsketch.travel.api.controller.notice;
 
 import com.travelsketch.travel.api.ApiResponse;
 import com.travelsketch.travel.api.controller.notice.request.CreateNoticeRequest;
+import com.travelsketch.travel.api.controller.notice.request.ModifyNoticeRequest;
 import com.travelsketch.travel.api.controller.notice.response.CreateNoticeResponse;
+import com.travelsketch.travel.api.controller.notice.response.ModifyNoticeResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+
+import static com.travelsketch.travel.api.ApiResponse.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,6 +29,17 @@ public class NoticeController {
             .createdDate(LocalDateTime.of(2023, 11, 26, 16, 46))
             .build();
 
-        return ApiResponse.created(response);
+        return created(response);
+    }
+
+    @PatchMapping("/{noticeId}")
+    public ApiResponse<ModifyNoticeResponse> modifyNotice(@PathVariable Long noticeId, @RequestBody ModifyNoticeRequest request) {
+        ModifyNoticeResponse response = ModifyNoticeResponse.builder()
+            .noticeId(1L)
+            .title("수정된 공지사항 제목입니다.")
+            .modifiedDate(LocalDateTime.of(2023, 11, 26, 16, 56))
+            .build();
+
+        return ok(response);
     }
 }
--- src/test/java/com/travelsketch/travel/docs/notice/NoticeControllerDocsTest.java
+++ src/test/java/com/travelsketch/travel/docs/notice/NoticeControllerDocsTest.java
@@ -2,6 +2,7 @@ package com.travelsketch.travel.docs.notice;
 
 import com.travelsketch.travel.api.controller.notice.NoticeController;
 import com.travelsketch.travel.api.controller.notice.request.CreateNoticeRequest;
+import com.travelsketch.travel.api.controller.notice.request.ModifyNoticeRequest;
 import com.travelsketch.travel.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,9 +11,14 @@ import org.springframework.restdocs.payload.JsonFieldType;
 
 import static com.travelsketch.travel.docs.ApiDocumentUtil.getDocumentRequest;
 import static com.travelsketch.travel.docs.ApiDocumentUtil.getDocumentResponse;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,6 +41,7 @@ public class NoticeControllerDocsTest extends RestDocsSupport {
 
         mockMvc.perform(
                 post(BASE_URL)
+                    .header("Authorization", "Bearer Access Token")
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(MediaType.APPLICATION_JSON)
             )
@@ -43,6 +50,10 @@ public class NoticeControllerDocsTest extends RestDocsSupport {
             .andDo(document("create-notice",
                 getDocumentRequest(),
                 getDocumentResponse(),
+                requestHeaders(
+                    headerWithName("Authorization")
+                        .description("Bearer Access Token")
+                ),
                 requestFields(
                     fieldWithPath("title").type(JsonFieldType.STRING)
                         .description("제목"),
@@ -64,6 +75,58 @@ public class NoticeControllerDocsTest extends RestDocsSupport {
                         .description("공지사항 제목"),
                     fieldWithPath("data.createdDate").type(JsonFieldType.ARRAY)
                         .description("공지사항 등록 일시")
+                )
+            ));
+    }
+
+    @DisplayName("공지사항 수정 API")
+    @Test
+    void modifyNotice() throws Exception {
+        ModifyNoticeRequest request = ModifyNoticeRequest.builder()
+            .title("수정된 공지사항 제목입니다.")
+            .content("수정된 공지사항 내용입니다.")
+            .build();
+
+        mockMvc.perform(
+                patch(BASE_URL + "/{noticeId}", 1L)
+                    .header("Authorization", "Bearer Access Token")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(document("modify-notice",
+                getDocumentRequest(),
+                getDocumentResponse(),
+                requestHeaders(
+                    headerWithName("Authorization")
+                        .description("Bearer Access Token")
+                ),
+                pathParameters(
+                    parameterWithName("noticeId")
+                        .description("공지사항 아이디")
+                ),
+                requestFields(
+                    fieldWithPath("title").type(JsonFieldType.STRING)
+                        .description("제목"),
+                    fieldWithPath("content").type(JsonFieldType.STRING)
+                        .description("내용")
+                ),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER)
+                        .description("코드"),
+                    fieldWithPath("status").type(JsonFieldType.STRING)
+                        .description("상태"),
+                    fieldWithPath("message").type(JsonFieldType.STRING)
+                        .description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT)
+                        .description("응답 데이터"),
+                    fieldWithPath("data.noticeId").type(JsonFieldType.NUMBER)
+                        .description("공지사항 아이디"),
+                    fieldWithPath("data.title").type(JsonFieldType.STRING)
+                        .description("공지사항 제목"),
+                    fieldWithPath("data.modifiedDate").type(JsonFieldType.ARRAY)
+                        .description("공지사항 수정 일시")
                 )
             ));
     }
