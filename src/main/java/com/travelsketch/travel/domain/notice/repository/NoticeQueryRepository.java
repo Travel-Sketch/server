82ac9479169c1ac53edae03becc7f10207258739
Chaos0103 <lyt1228@naver.com>
2023-11-27 00:51:45 +0900

    feat: 공지사항 목록 조회 구현

--- src/main/java/com/travelsketch/travel/domain/notice/repository/NoticeQueryRepository.java
+++ src/main/java/com/travelsketch/travel/domain/notice/repository/NoticeQueryRepository.java
@@ -1,13 +1,17 @@
 package com.travelsketch.travel.domain.notice.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.travelsketch.travel.api.controller.notice.response.NoticeResponse;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.travelsketch.travel.domain.notice.QNotice.notice;
 
 @Repository
 public class NoticeQueryRepository {
@@ -19,7 +23,37 @@ public class NoticeQueryRepository {
     }
 
     public List<NoticeResponse> findByCond(String query, Pageable pageable) {
-        return new ArrayList<>();
+        List<Long> ids = queryFactory
+            .select(notice.id)
+            .from(notice)
+            .where(
+                notice.isDeleted.isFalse(),
+                notice.title.like("%" + query + "%")
+            )
+            .orderBy(notice.createdDate.desc())
+            .limit(pageable.getPageSize())
+            .offset(pageable.getOffset())
+            .fetch();
+
+        if (CollectionUtils.isEmpty(ids)) {
+            return new ArrayList<>();
+        }
+
+        return queryFactory
+            .select(
+                Projections.constructor(
+                    NoticeResponse.class,
+                    notice.id,
+                    notice.title,
+                    notice.createdDate
+                )
+            )
+            .from(notice)
+            .where(
+                notice.id.in(ids)
+            )
+            .orderBy(notice.createdDate.desc())
+            .fetch();
     }
 
 }
