9ede252eebdf48fecbe5e8be0e81c060cb4bc8f7
Chaos0103 <lyt1228@naver.com>
2023-11-27 01:04:12 +0900

    feat: 조건에 맞는 공지사항 조회 서비스 구현

--- src/main/java/com/travelsketch/travel/api/service/notice/NoticeQueryService.java
+++ src/main/java/com/travelsketch/travel/api/service/notice/NoticeQueryService.java
@@ -4,9 +4,12 @@ import com.travelsketch.travel.api.PageResponse;
 import com.travelsketch.travel.api.controller.notice.response.NoticeResponse;
 import com.travelsketch.travel.domain.notice.repository.NoticeQueryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +19,12 @@ public class NoticeQueryService {
     private final NoticeQueryRepository noticeQueryRepository;
 
     public PageResponse<NoticeResponse> searchByCond(String query, Pageable pageable) {
-        return null;
+        List<NoticeResponse> responses = noticeQueryRepository.findByCond(query, pageable);
+
+        int count = noticeQueryRepository.findCountByCond(query);
+
+        PageImpl<NoticeResponse> content = new PageImpl<>(responses, pageable, count);
+
+        return new PageResponse<>(content);
     }
 }
