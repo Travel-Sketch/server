84dd41e1791b730e48d0c413ce080ece6e9db1e3
Chaos0103 <lyt1228@naver.com>
2023-11-27 14:14:11 +0900

    style: 주석 추가

--- src/main/java/com/travelsketch/travel/api/controller/member/MemberQueryController.java
+++ src/main/java/com/travelsketch/travel/api/controller/member/MemberQueryController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 회원 조회용 API
+ *
+ * @author 임우택
+ */
 @RequiredArgsConstructor
 @RestController
 @Slf4j
@@ -19,6 +24,11 @@ public class MemberQueryController {
     private final MemberQueryService memberQueryService;
     private final SecurityUtils securityUtils;
 
+    /**
+     * 회원 정보 조회 API
+     *
+     * @return 조회된 회원 정보
+     */
     @GetMapping
     public ApiResponse<MemberInfo> searchMemberInfo() {
         String email = securityUtils.getCurrentEmail();
