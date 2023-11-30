cde8abcae9c006da06f67c0882d14d331e340e10
Chaos0103 <lyt1228@naver.com>
2023-11-30 22:32:46 +0900

    feat: 조건에 따른 QnA 조회 서비스 구현

--- src/main/java/com/travelsketch/travel/api/service/qna/QnaQueryService.java
+++ src/main/java/com/travelsketch/travel/api/service/qna/QnaQueryService.java
@@ -4,9 +4,12 @@ import com.travelsketch.travel.api.PageResponse;
 import com.travelsketch.travel.api.controller.qna.response.QnaResponse;
 import com.travelsketch.travel.domain.qna.repository.QnaQueryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +19,12 @@ public class QnaQueryService {
     private final QnaQueryRepository qnaQueryRepository;
 
     public PageResponse<QnaResponse> searchQnas(String query, Pageable pageable) {
-        return null;
+        List<QnaResponse> responses = qnaQueryRepository.findByCond(query, pageable);
+
+        int count = qnaQueryRepository.findCountByCond(query);
+
+        PageImpl<QnaResponse> content = new PageImpl<>(responses, pageable, count);
+
+        return new PageResponse<>(content);
     }
 }
