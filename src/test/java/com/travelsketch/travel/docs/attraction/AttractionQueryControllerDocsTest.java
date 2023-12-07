8d3de6b3c82f32afaf98883889392d604cf068ec
Chaos0103 <lyt1228@naver.com>
2023-12-07 21:29:19 +0900

    feat: 관광지 목록 조회 API 컨트롤러 구현

--- src/test/java/com/travelsketch/travel/docs/attraction/AttractionQueryControllerDocsTest.java
+++ src/test/java/com/travelsketch/travel/docs/attraction/AttractionQueryControllerDocsTest.java
@@ -1,9 +1,11 @@
 package com.travelsketch.travel.docs.attraction;
 
 import com.travelsketch.travel.api.controller.attraction.AttractionQueryController;
+import com.travelsketch.travel.api.controller.attraction.response.AttractionResponse;
 import com.travelsketch.travel.api.controller.attraction.response.GugunResponse;
 import com.travelsketch.travel.api.controller.attraction.response.SidoResponse;
 import com.travelsketch.travel.api.service.attraction.AreaQueryService;
+import com.travelsketch.travel.api.service.attraction.AttractionQueryService;
 import com.travelsketch.travel.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,26 +14,28 @@ import org.springframework.restdocs.payload.JsonFieldType;
 import java.util.List;
 
 import static com.travelsketch.travel.docs.ApiDocumentUtil.getDocumentResponse;
+import static com.travelsketch.travel.domain.attraction.AttractionType.ATTRACTION;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AttractionQueryControllerDocsTest extends RestDocsSupport {
 
     private final AreaQueryService areaQueryService = mock(AreaQueryService.class);
-    private static final String BASE_URL = "/api/v1/sidos";
+    private final AttractionQueryService attractionQueryService = mock(AttractionQueryService.class);
+    private static final String BASE_URL = "/api/v1";
 
     @Override
     protected Object initController() {
-        return new AttractionQueryController(areaQueryService);
+        return new AttractionQueryController(areaQueryService, attractionQueryService);
     }
 
     @DisplayName("시도 목록 조회 API")
@@ -46,7 +50,7 @@ public class AttractionQueryControllerDocsTest extends RestDocsSupport {
             .willReturn(List.of(response));
 
         mockMvc.perform(
-                get(BASE_URL)
+                get(BASE_URL + "/sidos")
             )
             .andDo(print())
             .andExpect(status().isOk())
@@ -81,7 +85,7 @@ public class AttractionQueryControllerDocsTest extends RestDocsSupport {
             .willReturn(List.of(response));
 
         mockMvc.perform(
-                get(BASE_URL + "/{sidoId}/guguns", 1L)
+                get(BASE_URL + "/sidos/{sidoId}/guguns", 1L)
             )
             .andDo(print())
             .andExpect(status().isOk())
@@ -111,18 +115,41 @@ public class AttractionQueryControllerDocsTest extends RestDocsSupport {
     @DisplayName("관광지 목록 조회 API")
     @Test
     void searchAttractions() throws Exception {
+        AttractionResponse response = AttractionResponse.builder()
+            .attractionId(126498)
+            .type(ATTRACTION.getText())
+            .title("롯데월드")
+            .zipcode("5554")
+            .address("서울특별시 송파구 올림픽로 240")
+            .tel(null)
+            .image("http://tong.visitkorea.or.kr/cms/resource/36/2540136_image2_1.JPG")
+            .longitude(127.09793950000000000)
+            .latitude(37.51122948000000000)
+            .build();
+
+        given(attractionQueryService.searchAttraction(anyInt(), anyInt(), anyInt(), anyString()))
+            .willReturn(List.of(response));
+
         mockMvc.perform(
-                get(BASE_URL + "/{sidoId}/guguns/{gugunId}/attractions", 1L, 1L)
+                get(BASE_URL + "/attractions")
+                    .queryParam("codeId", "1")
+                    .queryParam("gugunId", "1")
+                    .queryParam("typeId", "12")
+                    .queryParam("query", "롯데")
             )
             .andDo(print())
             .andExpect(status().isOk())
             .andDo(document("search-attractions",
                 getDocumentResponse(),
-                pathParameters(
-                    parameterWithName("sidoId")
+                queryParameters(
+                    parameterWithName("codeId")
                         .description("시도 아이디"),
                     parameterWithName("gugunId")
-                        .description("구군 아이디")
+                        .description("구군 아이디"),
+                    parameterWithName("typeId")
+                        .description("관광지 유형 아이디"),
+                    parameterWithName("query")
+                        .description("검색 쿼리")
                 ),
                 responseFields(
                     fieldWithPath("code").type(JsonFieldType.NUMBER)
@@ -134,6 +161,8 @@ public class AttractionQueryControllerDocsTest extends RestDocsSupport {
                     fieldWithPath("data").type(JsonFieldType.ARRAY)
                         .description("응답 데이터"),
                     fieldWithPath("data[].attractionId").type(JsonFieldType.NUMBER)
+                        .description("관광지 아이디"),
+                    fieldWithPath("data[].type").type(JsonFieldType.STRING)
                         .description("관광지 아이디"),
                     fieldWithPath("data[].title").type(JsonFieldType.STRING)
                         .description("관광지 제목"),
