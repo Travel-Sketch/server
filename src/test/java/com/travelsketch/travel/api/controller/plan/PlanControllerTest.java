d4cef680d88e6541b4ea9ba45b7568c529a297f7
Chaos0103 <lyt1228@naver.com>
2023-12-11 16:14:25 +0900

    test: 관광지 입력 갯수 검증 조건 테스트 코드 구현

--- src/test/java/com/travelsketch/travel/api/controller/plan/PlanControllerTest.java
+++ src/test/java/com/travelsketch/travel/api/controller/plan/PlanControllerTest.java
@@ -87,6 +87,29 @@ class PlanControllerTest extends ControllerTestSupport {
             .andExpect(jsonPath("$.message").value("관광지 정보는 최소 1개 이상 입력해주세요."));
     }
 
+    @DisplayName("계획 등록시 관광지 정보는 최대 10개이다.")
+    @Test
+    void createPlanOutOfMaxSizeAttractions() throws Exception {
+        //given
+        CreatePlanRequest request = CreatePlanRequest.builder()
+            .title("여행 계획 제목입니다.")
+            .attractions(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
+            .build();
+
+        //when //then
+        mockMvc.perform(
+                post(BASE_URL)
+                    .with(csrf())
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$.message").value("등록할 수 있는 관광지 수는 최대 10개입니다."));
+    }
+
     private String getText(int size) {
         return "a".repeat(Math.max(0, size));
     }
