d9db44bfecf9039a56c137e1c2a9a164c495bfb0
Chaos0103 <lyt1228@naver.com>
2023-12-12 01:51:37 +0900

    test: 여행 계획 수정 API 컨트롤러 테스트 코드 구현

--- src/test/java/com/travelsketch/travel/api/controller/plan/PlanControllerTest.java
+++ src/test/java/com/travelsketch/travel/api/controller/plan/PlanControllerTest.java
@@ -2,12 +2,14 @@ package com.travelsketch.travel.api.controller.plan;
 
 import com.travelsketch.travel.ControllerTestSupport;
 import com.travelsketch.travel.api.controller.plan.request.CreatePlanRequest;
+import com.travelsketch.travel.api.controller.plan.request.ModifyPlanRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
 import java.util.List;
 
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -99,6 +101,98 @@ class PlanControllerTest extends ControllerTestSupport {
         //when //then
         mockMvc.perform(
                 post(BASE_URL)
+                    .with(csrf())
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$.message").value("등록할 수 있는 관광지 수는 최대 10개입니다."));
+    }
+
+    @DisplayName("계획 수정시 제목은 필수값이다.")
+    @Test
+    void modifyPlanWithoutTitle() throws Exception {
+        //given
+        ModifyPlanRequest request = ModifyPlanRequest.builder()
+            .title(" ")
+            .attractions(List.of(1, 2, 3))
+            .build();
+
+        //when //then
+        mockMvc.perform(
+                patch(BASE_URL + "/{planId}", 1)
+                    .with(csrf())
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$.message").value("제목은 필수입니다."));
+    }
+
+    @DisplayName("계획 수정시 제목은 최대 50자이다.")
+    @Test
+    void modifyPlanOutOfMaxSizeTitle() throws Exception {
+        //given
+        ModifyPlanRequest request = ModifyPlanRequest.builder()
+            .title(getText(51))
+            .attractions(List.of(1, 2, 3))
+            .build();
+
+        //when //then
+        mockMvc.perform(
+                patch(BASE_URL + "/{planId}", 1)
+                    .with(csrf())
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$.message").value("제목은 최대 50자입니다."));
+    }
+
+    @DisplayName("계획 수정시 관광지 정보는 최소 1개 이상이다.")
+    @Test
+    void modifyPlanWithoutAttractions() throws Exception {
+        //given
+        ModifyPlanRequest request = ModifyPlanRequest.builder()
+            .title("여행 계획 제목입니다.")
+            .attractions(List.of())
+            .build();
+
+        //when //then
+        mockMvc.perform(
+                patch(BASE_URL + "/{planId}", 1)
+                    .with(csrf())
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+            .andExpect(jsonPath("$.message").value("관광지 정보는 최소 1개 이상 입력해주세요."));
+    }
+
+    @DisplayName("계획 수정시 관광지 정보는 최대 10개이다.")
+    @Test
+    void modifyPlanOutOfMaxSizeAttractions() throws Exception {
+        //given
+        ModifyPlanRequest request = ModifyPlanRequest.builder()
+            .title("여행 계획 제목입니다.")
+            .attractions(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
+            .build();
+
+        //when //then
+        mockMvc.perform(
+                patch(BASE_URL + "/{planId}", 1)
                     .with(csrf())
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(MediaType.APPLICATION_JSON)
