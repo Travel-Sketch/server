aa9c088cbd9bfd2143d01c3e697277cfe8d6afcb
ji-hyon <gyg03134@gmail.com>
2023-12-17 02:27:32 +0900

    test: 게시물 수정 테스트 코드

--- src/main/java/com/travelsketch/travel/api/service/board/PostService.java
+++ src/main/java/com/travelsketch/travel/api/service/board/PostService.java
@@ -1,12 +1,15 @@
 package com.travelsketch.travel.api.service.board;
 
 import com.travelsketch.travel.api.controller.board.response.CreatePostResponse;
+import com.travelsketch.travel.api.controller.board.response.ModifyPostResponse;
+import com.travelsketch.travel.api.controller.notice.response.ModifyNoticeResponse;
 import com.travelsketch.travel.domain.board.Post;
 import com.travelsketch.travel.domain.board.PostCategory;
 import com.travelsketch.travel.domain.board.UploadFile;
 import com.travelsketch.travel.domain.board.repository.PostRepository;
 import com.travelsketch.travel.domain.member.Member;
 import com.travelsketch.travel.domain.member.repository.MemberRepository;
+import com.travelsketch.travel.domain.notice.Notice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,14 +32,15 @@ public class PostService {
             throw new NoSuchElementException("등록되지 않은 회원입니다.");
         }
         Member member = findMember.get();
-
         Post post = Post.createPost(PostCategory.FREE, title, content, member, files);
-
         Post savedPost = postRepository.save(post);
 
         return CreatePostResponse.of(savedPost, files);
-
     }
 
+    public ModifyPostResponse modifyPost(String email, Long postId, String title, String content, List<UploadFile> files) {
+
+        return null;
+    }
 
 }
--- src/test/java/com/travelsketch/travel/api/service/board/PostServiceTest.java
+++ src/test/java/com/travelsketch/travel/api/service/board/PostServiceTest.java
@@ -2,7 +2,9 @@ package com.travelsketch.travel.api.service.board;
 
 import com.travelsketch.travel.IntegrationTestSupport;
 import com.travelsketch.travel.api.controller.board.response.CreatePostResponse;
+import com.travelsketch.travel.api.controller.board.response.ModifyPostResponse;
 import com.travelsketch.travel.domain.board.Post;
+import com.travelsketch.travel.domain.board.PostCategory;
 import com.travelsketch.travel.domain.board.repository.PostRepository;
 import com.travelsketch.travel.domain.member.Member;
 import com.travelsketch.travel.domain.member.Role;
@@ -27,8 +29,7 @@ class PostServiceTest extends IntegrationTestSupport {
     @Autowired
     private MemberRepository memberRepository;
 
-
-    @DisplayName("제목, 내용을 입력 받아 게시물을 등록할 수 있다.")
+    @DisplayName("제목, 내용, 첨부파일을 입력 받아 게시물을 등록할 수 있다.")
     @Test
     void createPost() {
         // given
@@ -49,6 +50,40 @@ class PostServiceTest extends IntegrationTestSupport {
         // then (post 등록 확인)
         Optional<Post> findPost = postRepository.findById(response.getPostId());
         assertThat(findPost).isPresent();
+    }
+
+    @DisplayName("제목, 내용을 입력 받아 게시물을 수정할 수 있다.")
+    @Test
+    void modifyPost() {
+        // given
+        Member member = Member.builder()
+            .email("cherry@naver.com")
+            .pwd(passwordEncoder.encode("cherry_password"))
+            .name("서지현")
+            .birth("2000-01-01")
+            .gender("F")
+            .nickname("체리")
+            .role(Role.USER)
+            .build();
+        memberRepository.save(member);
+
+        Post post = Post.builder()
+            .category(PostCategory.FREE)
+            .title("게시물 제목1")
+            .content("게시물 내용1")
+            .scrapCount(0)
+            .commentCount(0)
+            .member(member)
+            .build();
+        postRepository.save(post);
+
+        // when
+        ModifyPostResponse response = postService.modifyPost(member.getEmail(), post.getId(), "게시물 제목", "게시물 내용 수정", List.of());
+
+        // then
+        Optional<Post> findPost = postRepository.findById(response.getPostId());
+        assertThat(findPost).isPresent();
+        assertThat(findPost.get().getContent()).isEqualTo("게시물 내용 수정");
     }
 
 }
