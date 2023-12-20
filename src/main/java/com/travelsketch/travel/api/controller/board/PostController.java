1aa8c037adbd7670998ce3393ab82b3fd7760afb
ji-hyon <gyg03134@gmail.com>
2023-12-21 00:47:22 +0900

    refactor: 업로드 파일명 길이 체크 함수 extract

--- src/main/java/com/travelsketch/travel/api/controller/board/PostController.java
+++ src/main/java/com/travelsketch/travel/api/controller/board/PostController.java
@@ -30,24 +30,19 @@ public class PostController {
     private final SecurityUtils securityUtils;
     private final PostService postService;
     private final FileStore fileStore;
-
+    
     /**
      * 게시물 등록 API
-     *
+     * 
      * @param request 등록할 게시물 정보
      * @return 등록된 게시물 정보
+     * @throws IOException
      */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<CreatePostResponse> createPost(@Valid @ModelAttribute CreatePostRequest request) throws IOException {
-        if (request.getFiles() != null) {
-            for (MultipartFile file : request.getFiles()) {
-                if (file.getOriginalFilename() == null) continue;
-                if (file.getOriginalFilename().length() > 100) {
-                    throw new IllegalArgumentException("업로드 파일명 길이가 허용된 최대 크기를 초과했습니다.");
-                }
-            }
-        }
+        checkFileNameLength(request.getFiles());
+
         String email = securityUtils.getCurrentEmail();
 
         List<UploadFile> uploadFiles = fileStore.storeFiles(request.getFiles());
@@ -59,9 +54,11 @@ public class PostController {
 
     /**
      * 게시물 수정 API
-     *
+     * 
+     * @param postId 게시물 id
      * @param request 게시물 수정 내용
      * @return 게시물 수정 내용
+     * @throws IOException
      */
     @PatchMapping("/{postId}")
     public ApiResponse<ModifyPostResponse> modifyPost(
@@ -69,14 +66,7 @@ public class PostController {
         @Valid
         @ModelAttribute ModifyPostRequest request
     ) throws IOException {
-        if (request.getNewFiles() != null) {
-            for (MultipartFile file : request.getNewFiles()) {
-                if (file.getOriginalFilename() == null) continue;
-                if (file.getOriginalFilename().length() > 100) {
-                    throw new IllegalArgumentException("업로드 파일명 길이가 허용된 최대 크기를 초과했습니다.");
-                }
-            }
-        }
+        checkFileNameLength(request.getNewFiles());
 
         String email = securityUtils.getCurrentEmail();
 
@@ -86,10 +76,11 @@ public class PostController {
 
         return ok(response);
     }
-
+    
     /**
      * 게시물 삭제 API
-     *
+     * 
+     * @param postId 게시물 id
      * @return 삭제된 게시물
      */
     @DeleteMapping("/{postId}")
@@ -100,6 +91,22 @@ public class PostController {
             .isDeleted(true)
             .build();
         return ok(response);
+    }
+
+    /**
+     * 업로드 파일명 길이 체크 함수
+     *
+     * @param files 업로드 파일 정보
+     */
+    private static void checkFileNameLength(List<MultipartFile> files) {
+        if (files != null) {
+            for (MultipartFile file : files) {
+                if (file.getOriginalFilename() == null) continue;
+                if (file.getOriginalFilename().length() > 100) {
+                    throw new IllegalArgumentException("업로드 파일명 길이가 허용된 최대 크기를 초과했습니다.");
+                }
+            }
+        }
     }
 
 }
