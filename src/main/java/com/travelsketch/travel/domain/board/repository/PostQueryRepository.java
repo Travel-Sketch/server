a8fcc5416b3e986c2e7cd4a2e935413185106cab
ji-hyon <gyg03134@gmail.com>
2023-12-26 22:27:55 +0900

    test: 게시물 목록 조회 테스트 코드

--- src/main/java/com/travelsketch/travel/domain/board/repository/PostQueryRepository.java
+++ src/main/java/com/travelsketch/travel/domain/board/repository/PostQueryRepository.java
@@ -1,17 +1,17 @@
 package com.travelsketch.travel.domain.board.repository;
 
-import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.travelsketch.travel.api.controller.notice.response.NoticeDetailResponse;
+import com.travelsketch.travel.api.controller.board.response.SearchPostsResponse;
 import com.travelsketch.travel.domain.board.Post;
 import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static com.travelsketch.travel.domain.board.QPost.post;
-import static com.travelsketch.travel.domain.notice.QNotice.notice;
 
 @Repository
 public class PostQueryRepository {
@@ -20,6 +20,10 @@ public class PostQueryRepository {
 
     public PostQueryRepository(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<SearchPostsResponse> findByCriteria(PageRequest pageRequest, String query) {
+        return new ArrayList<>();
     }
 
     public Optional<Post> findByIdWithMember(Long postId) {
---
+++ src/test/java/com/travelsketch/travel/domain/board/repository/PostQueryRepositoryTest.java
@@ -0,0 +1,101 @@
+package com.travelsketch.travel.domain.board.repository;
+
+import com.travelsketch.travel.IntegrationTestSupport;
+import com.travelsketch.travel.api.controller.board.response.SearchPostsResponse;
+import com.travelsketch.travel.domain.board.Post;
+import com.travelsketch.travel.domain.board.PostCategory;
+import com.travelsketch.travel.domain.member.Member;
+import com.travelsketch.travel.domain.member.Role;
+import com.travelsketch.travel.domain.member.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+import static com.travelsketch.travel.domain.board.Post.createPost;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+class PostQueryRepositoryTest extends IntegrationTestSupport {
+
+    @Autowired
+    private PostQueryRepository postQueryRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @DisplayName("페이지 번호를 입력 받아 게시물 목록을 조회한다.")
+    @Test
+    void findByPage() {
+        //given
+        Member member = Member.builder()
+            .email("cherry@naver.com")
+            .pwd(passwordEncoder.encode("cherry_password"))
+            .name("서지현")
+            .birth("2000-01-01")
+            .gender("F")
+            .nickname("체리")
+            .role(Role.USER)
+            .build();
+        memberRepository.save(member);
+
+        Post post1 = createPost(PostCategory.FREE, "게시글1 제목", "게시글1 내용", member, List.of());
+        Post post2 = createPost(PostCategory.FREE, "게시글2 제목", "게시글2 내용", member, List.of());
+        Post post3 = createPost(PostCategory.FREE, "게시글3 제목", "게시글3 내용", member, List.of());
+        Post post4 = createPost(PostCategory.FREE, "게시글4", "게시글4", member, List.of());
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        //when
+        List<SearchPostsResponse> responses = postQueryRepository.findByCriteria(pageRequest, "");
+
+        //then
+        assertThat(responses).hasSize(4)
+            .extracting("postId", "title")
+            .containsExactlyInAnyOrder(
+                tuple(post1.getId(), "게시글1 제목"),
+                tuple(post2.getId(), "게시글2 제목"),
+                tuple(post3.getId(), "게시글3 제목"),
+                tuple(post4.getId(), "게시글4")
+            );
+
+    }
+
+    @DisplayName("검색 쿼리를 추가적으로 입력할 수 있다.")
+    @Test
+    void findByCriteria() {
+        //given
+        Member member = Member.builder()
+            .email("cherry@naver.com")
+            .pwd(passwordEncoder.encode("cherry_password"))
+            .name("서지현")
+            .birth("2000-01-01")
+            .gender("F")
+            .nickname("체리")
+            .role(Role.USER)
+            .build();
+        memberRepository.save(member);
+
+        Post post1 = createPost(PostCategory.FREE, "게시글1 제목", "게시글1 내용", member, List.of());
+        Post post2 = createPost(PostCategory.FREE, "게시글2 제목", "게시글2 내용", member, List.of());
+        Post post3 = createPost(PostCategory.FREE, "게시글3 제목", "게시글3 내용", member, List.of());
+        Post post4 = createPost(PostCategory.FREE, "게시글4", "게시글4", member, List.of());
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        //when
+        List<SearchPostsResponse> responses = postQueryRepository.findByCriteria(pageRequest, "제목");
+
+        //then
+        assertThat(responses).hasSize(3)
+            .extracting("postId", "title")
+            .containsExactlyInAnyOrder(
+                tuple(post1.getId(), "게시글1 제목"),
+                tuple(post2.getId(), "게시글2 제목"),
+                tuple(post3.getId(), "게시글3 제목")
+            );
+    }
+
+}
