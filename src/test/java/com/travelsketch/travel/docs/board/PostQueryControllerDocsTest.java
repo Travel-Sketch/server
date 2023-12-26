3ee93e05a8c8823dbab3ce5b0548454edbc5eaa8
ji-hyon <gyg03134@gmail.com>
2023-12-27 00:36:49 +0900

    fix: 게시물 목록 조회 DocsTest 수정

--- src/test/java/com/travelsketch/travel/docs/board/PostQueryControllerDocsTest.java
+++ src/test/java/com/travelsketch/travel/docs/board/PostQueryControllerDocsTest.java
@@ -1,15 +1,25 @@
 package com.travelsketch.travel.docs.board;
 
+import com.travelsketch.travel.api.PageResponse;
 import com.travelsketch.travel.api.controller.board.PostQueryController;
+import com.travelsketch.travel.api.controller.board.response.SearchPostsResponse;
 import com.travelsketch.travel.api.service.board.PostQueryService;
 import com.travelsketch.travel.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import static com.travelsketch.travel.docs.ApiDocumentUtil.getDocumentRequest;
 import static com.travelsketch.travel.docs.ApiDocumentUtil.getDocumentResponse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
@@ -32,6 +42,30 @@ public class PostQueryControllerDocsTest extends RestDocsSupport {
     @DisplayName("게시물 목록 조회 API")
     @Test
     void searchPosts() throws Exception {
+
+        SearchPostsResponse response1 = SearchPostsResponse.builder()
+            .postId(1L)
+            .title("게시물 제목")
+            .createdDate(LocalDateTime.of(2023, 12, 7, 10, 30))
+            .build();
+        SearchPostsResponse response2 = SearchPostsResponse.builder()
+            .postId(2L)
+            .title("게시물 제목2")
+            .createdDate(LocalDateTime.of(2023, 12, 27, 10, 40))
+            .build();
+        SearchPostsResponse response3 = SearchPostsResponse.builder()
+            .postId(3L)
+            .title("게시물 제목3")
+            .createdDate(LocalDateTime.of(2023, 12, 27, 10, 40))
+            .build();
+        List<SearchPostsResponse> responses = List.of(response1, response2, response3);
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        PageImpl<SearchPostsResponse> content = new PageImpl<>(responses, pageRequest, responses.size());
+
+        given(postQueryService.searchByCriteria(any(), anyString()))
+            .willReturn(new PageResponse<>(content));
 
         mockMvc.perform(get(BASE_URL + "?page=1&query=검색어")
                 .contentType(MediaType.APPLICATION_JSON)
