86d6feacbb448095f3e4f2cd5e67692bde2618a9
ji-hyon <gyg03134@gmail.com>
2023-12-26 22:59:56 +0900

    feat: 게시물 목록 조회 기능

--- src/main/java/com/travelsketch/travel/domain/board/repository/PostQueryRepository.java
+++ src/main/java/com/travelsketch/travel/domain/board/repository/PostQueryRepository.java
@@ -1,11 +1,13 @@
 package com.travelsketch.travel.domain.board.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.travelsketch.travel.api.controller.board.response.SearchPostsResponse;
 import com.travelsketch.travel.domain.board.Post;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +25,37 @@ public class PostQueryRepository {
     }
 
     public List<SearchPostsResponse> findByCriteria(PageRequest pageRequest, String query) {
-        return new ArrayList<>();
+        List<Long> postIds = queryFactory
+            .select(post.id)
+            .from(post)
+            .where(
+                post.isDeleted.isFalse(),
+                post.title.like("%" + query + "%")
+            )
+            .orderBy(post.createdDate.desc())
+            .limit(pageRequest.getPageSize())
+            .offset(pageRequest.getOffset())
+            .fetch();
+
+        if (postIds.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return queryFactory
+            .select(
+                Projections.constructor(
+                    SearchPostsResponse.class,
+                    post.id,
+                    post.title,
+                    post.createdDate
+                )
+            )
+            .from(post)
+            .where(
+                post.id.in(postIds)
+            )
+            .orderBy(post.createdDate.desc())
+            .fetch();
     }
 
     public Optional<Post> findByIdWithMember(Long postId) {
