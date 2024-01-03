96687454008a56caf99e7d73dfbbe61370b8e2ee
ji-hyon <gyg03134@gmail.com>
2024-01-03 22:00:31 +0900

    test: 게시물 댓글 등록 예외 테스트 코드 추가

--- src/test/java/com/travelsketch/travel/api/service/board/CommentServiceTest.java
+++ src/test/java/com/travelsketch/travel/api/service/board/CommentServiceTest.java
@@ -15,11 +15,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static com.travelsketch.travel.domain.board.Post.createPost;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CommentServiceTest extends IntegrationTestSupport {
 
@@ -70,7 +71,76 @@ class CommentServiceTest extends IntegrationTestSupport {
         // then
         Optional<Comment> findComment = commentRepository.findById(response.getCommentId());
         assertThat(findComment).isPresent();
+    }
 
+    @DisplayName("입력 받은 상위 댓글 id가 유효하지 않으면 예외가 발생한다.")
+    @Test
+    void createCommentWithInvalidParentCommentId() {
+        // given
+        Member member1 = Member.builder()
+            .email("cherry@naver.com")
+            .pwd(passwordEncoder.encode("cherry_password"))
+            .name("서지현")
+            .birth("2010-01-01")
+            .gender("F")
+            .nickname("체리")
+            .role(Role.USER)
+            .build();
+        memberRepository.save(member1);
+
+        Member member2 = Member.builder()
+            .email("apple@naver.com")
+            .pwd(passwordEncoder.encode("apple_password"))
+            .name("서현지")
+            .birth("2011-11-11")
+            .gender("F")
+            .nickname("사과")
+            .role(Role.USER)
+            .build();
+        memberRepository.save(member2);
+
+        Post post = createPost(PostCategory.FREE, "게시물 제목", "게시물 내용", member1, List.of());
+        postRepository.save(post);
+
+        //when //then
+        assertThatThrownBy(() -> commentService.createComment(member2.getEmail(), 1L, 123L, ""))
+            .isInstanceOf(NoSuchElementException.class)
+            .hasMessage("상위 댓글을 찾을 수 없습니다.");
+    }
+
+    @DisplayName("입력 받은 게시글 id가 유효하지 않으면 예외가 발생한다.")
+    @Test
+    void createCommentWithInvalidPostId() {
+        // given
+        Member member1 = Member.builder()
+            .email("cherry@naver.com")
+            .pwd(passwordEncoder.encode("cherry_password"))
+            .name("서지현")
+            .birth("2010-01-01")
+            .gender("F")
+            .nickname("체리")
+            .role(Role.USER)
+            .build();
+        memberRepository.save(member1);
+
+        Member member2 = Member.builder()
+            .email("apple@naver.com")
+            .pwd(passwordEncoder.encode("apple_password"))
+            .name("서현지")
+            .birth("2011-11-11")
+            .gender("F")
+            .nickname("사과")
+            .role(Role.USER)
+            .build();
+        memberRepository.save(member2);
+
+        Post post = createPost(PostCategory.FREE, "게시물 제목", "게시물 내용", member1, List.of());
+        postRepository.save(post);
+
+        //when //then
+        assertThatThrownBy(() -> commentService.createComment(member2.getEmail(), 123L, null, ""))
+            .isInstanceOf(NoSuchElementException.class)
+            .hasMessage("등록되지 않은 게시물입니다.");
     }
 
 }
