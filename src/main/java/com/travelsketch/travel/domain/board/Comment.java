c57f8f6fe7641ccf762331b4695acb35190219b4
ji-hyon <gyg03134@gmail.com>
2024-01-03 20:08:56 +0900

    feat: 댓글 엔티티 작성

---
+++ src/main/java/com/travelsketch/travel/domain/board/Comment.java
@@ -0,0 +1,36 @@
+package com.travelsketch.travel.domain.board;
+
+import com.travelsketch.travel.domain.TimeBaseEntity;
+import com.travelsketch.travel.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+}
